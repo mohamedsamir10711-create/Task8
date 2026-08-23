04835ce2f4e13eb3d5605559a7893dc3f2ed0649
mohamedsamir10711-create <mohamedsamir10711@gmail.com>
2026-08-23 04:44:15 +0300

    added Pins header file

---
+++ pins.h
@@ -0,0 +1,17 @@
+#ifndef PINS_H
+#define PINS_H
+
+// L298N Pins
+#define L298N_IN1 PA0
+#define L298N_IN2 PA1
+#define L298N_ENA PA2
+
+// Cytron MD10A Pins
+#define CYTRON_DIR PA3
+#define CYTRON_PWM PA6
+
+// BTS7960 Pins
+#define BTS_RPWM PB6
+#define BTS_LPWM PB7
+
+#endif
